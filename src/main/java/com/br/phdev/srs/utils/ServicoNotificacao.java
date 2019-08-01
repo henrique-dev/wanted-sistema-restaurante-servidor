4f008c639d2a4dbbe14b22f69f7c38422ed984fc
Paulo Henrique <henrique.phgb@gmail.com>
2019-08-01 02:20:49 -0300

    fixs

--- src/main/java/com/br/phdev/srs/utils/ServicoNotificacao.java
+++ src/main/java/com/br/phdev/srs/utils/ServicoNotificacao.java
@@ -6,55 +6,26 @@
  */
 package com.br.phdev.srs.utils;
 
-import com.br.phdev.srs.models.Cliente;
-import com.google.gson.Gson;
 import java.io.IOException;
-import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.messaging.converter.MessageConverter;
-import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
-import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 /**
  *
  * @author Paulo Henrique Gonçalves Bacelar <henrique.phgb@gmail.com>
  */
-//@Configuration
-//@EnableWebSocket
-public class ServicoNotificacao /*implements WebSocketConfigurer*/ {
-        /*
+@Configuration
+@EnableWebSocket
+public class ServicoNotificacao implements WebSocketConfigurer {
+        
     private static List<WebSocketSession> sessions = new ArrayList();
     
     synchronized public static List<WebSocketSession> getSessions() {
@@ -74,13 +45,13 @@ public class ServicoNotificacao /*implements WebSocketConfigurer*/ {
                 throws InterruptedException, IOException {            
             
             System.out.println(message.getPayload());
-            
+            /*
             for (WebSocketSession webSocketSession : sessions) {
                 System.out.println(webSocketSession.getId());
                 System.out.println(webSocketSession.isOpen());
                 Map value = new Gson().fromJson(message.getPayload(), Map.class);
                 webSocketSession.sendMessage(new TextMessage("OLA MUNDO"));
-            }
+            }*/
         }
 
         @Override
@@ -89,6 +60,6 @@ public class ServicoNotificacao /*implements WebSocketConfigurer*/ {
             sessions.add(session);
             session.sendMessage(new TextMessage(session.getId()));            
         }
-    }*/
+    }
 
 }
--- src/main/java/com/br/phdev/srs/utils/SevicoAgendamento.java
+++ src/main/java/com/br/phdev/srs/utils/SevicoAgendamento.java
@@ -1,12 +1,8 @@
 package com.br.phdev.srs.utils;
 
-import com.br.phdev.srs.models.Mensagem;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -17,18 +13,17 @@ import org.springframework.web.socket.WebSocketSession;
  *
  * @author henrique
  */
-//@Component
-//@EnableScheduling
+@Component
+@EnableScheduling
 public class SevicoAgendamento {
-//Atributos        
 
-    //@Scheduled(fixedDelay = 5000)
+    @Scheduled(fixedDelay = 5000)
     public void verificaPorHora() throws JsonProcessingException, IOException {
-        //List<WebSocketSession> sessions = ServicoNotificacao.getSessions();
-        //for (WebSocketSession s : sessions) {
-            //s.sendMessage(new TextMessage("Testando envio de mensagens"));
-            //System.out.println("Enviou msg para: " + s);
-        //}
+        List<WebSocketSession> sessions = ServicoNotificacao.getSessions();
+        for (WebSocketSession s : sessions) {
+            s.sendMessage(new TextMessage("Testando envio de mensagens"));
+            System.out.println("Enviou msg para: " + s);
+        }
     }
 
 }
