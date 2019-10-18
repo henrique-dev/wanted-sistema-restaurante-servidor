3fd52b1ff0118d9a408dba964b1cae4155c35c3c
Paulo Henrique <henrique.phgb@gmail.com>
2019-10-17 23:08:10 -0300

    varios fixs

--- src/main/java/com/br/phdev/srs/utils/ServicoPagamentoPagarme.java
+++ src/main/java/com/br/phdev/srs/utils/ServicoPagamentoPagarme.java
@@ -9,6 +9,7 @@ package com.br.phdev.srs.utils;
 
 import com.br.phdev.srs.models.Cartao;
 import com.br.phdev.srs.models.ExecutarPagamento;
+import com.br.phdev.srs.models.ItemPedidoFacil;
 import com.br.phdev.srs.models.Pedido;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -31,12 +32,12 @@ public class ServicoPagamentoPagarme {
 
     public static final String apiKey = "ak_live_W0ndrvfrr5lQMAkP0UAGYxw0NgFPen";
     public static final String ENCRYPT_KEY = "ek_live_zIZULHGU8MABqVcfYhcySYc688O3bg";
-    
+
     public ServicoPagamentoPagarme() {
         PagarMe.init(apiKey);
     }
 
-    public Card salvarCartao(Cartao cartao) throws PagarMeException {        
+    public Card salvarCartao(Cartao cartao) throws PagarMeException {
         Card c = new Card();
         c.setNumber(cartao.getNumero());
         c.setHolderName(cartao.getNome());
@@ -45,11 +46,11 @@ public class ServicoPagamentoPagarme {
         c.save();
         return c;
     }
-    
+
     public Card getCartao(String hashID) throws PagarMeException {
         return new Card().find(hashID);
     }
-    
+
     public Transaction gerarRembolso(Pedido pedido) throws PagarMeException {
         Transaction transaction = new Transaction().find(pedido.getToken());
         //System.out.println("Valor: " +  Integer.parseInt(String.valueOf(pedido.getPrecoTotal()).replace(".", "")));
@@ -57,7 +58,7 @@ public class ServicoPagamentoPagarme {
         return transaction;
     }
 
-    public Transaction criarPagamento2(String token) throws PagarMeException {        
+    public Transaction criarPagamento2(String token) throws PagarMeException {
         Transaction transaction = new Transaction();
         try {
 
@@ -128,56 +129,56 @@ public class ServicoPagamentoPagarme {
             Customer customer = new Customer();
             customer.setType(Customer.Type.INDIVIDUAL);
             customer.setExternalId("1001");
-            customer.setName("Phineas Flynn");
-            customer.setBirthday("1999-07-09");
-            customer.setEmail("phineas@threestatearea.com");
+            customer.setName(pagamento.getCliente().getNome());
+            //customer.setBirthday(paga);
+            customer.setEmail(pagamento.getCliente().getEmail());
             customer.setCountry("br");
 
             Collection<Document> documents = new ArrayList();
             Document document = new Document();
             document.setType(Document.Type.CPF);
-            document.setNumber("77551442758");
+            document.setNumber(pagamento.getCliente().getCpf());
             documents.add(document);
             customer.setDocuments(documents);
 
             Collection<String> phones = new ArrayList();
-            phones.add("+5511982657575");
+            phones.add(pagamento.getCliente().getTelefone());
             customer.setPhoneNumbers(phones);
 
             Billing billing = new Billing();
-            billing.setName("Phineas Flynn");
+            billing.setName(pagamento.getCliente().getNome());
             Address address = new Address();
-            address.setCity("Santo Andre");
+            address.setCity(pagamento.getEndereco().getCidade());
             address.setCountry("br");
-            address.setState("sp");
-            address.setNeighborhood("Parque Miami");
-            address.setStreet("Rua Rio Jari");
-            address.setZipcode("09133180");
-            address.setStreetNumber("7");
+            address.setState("ap");
+            address.setNeighborhood(pagamento.getEndereco().getBairro());
+            address.setStreet(pagamento.getEndereco().getLogradouro());
+            address.setZipcode(pagamento.getEndereco().getCep());
+            address.setStreetNumber(pagamento.getEndereco().getNumero());
             billing.setAddress(address);
 
             Shipping shipping = new Shipping();
             shipping.setAddress(address);
-            shipping.setName("Phineas Flynn");
-            shipping.setFee(3200);
+            shipping.setName(pagamento.getCliente().getNome());
+            //shipping.setFee(3200);
 
             Collection<Item> items = new ArrayList();
-            Item item = new Item();
-            item.setId("OX890");
-            item.setQuantity(12);
-            item.setTangible(Boolean.TRUE);
-            item.setTitle("Rockets");
-            item.setUnitPrice(120);
+            for (ItemPedidoFacil ipf : pagamento.getPedido().getItens()) {
+                Item item = new Item();
+                item.setId(String.valueOf(ipf.getId()));
+                item.setQuantity(ipf.getQuantidade());
+                item.setTangible(Boolean.TRUE);
+                item.setTitle(ipf.getNome());
+                item.setUnitPrice(Integer.parseInt(String.valueOf(ipf.getPrecoTotal()).replace(".", "")));
+                items.add(item);
+            }
 
             transaction.setShipping(shipping);
             transaction.setBilling(billing);
             transaction.setItems(items);
             transaction.setPaymentMethod(Transaction.PaymentMethod.CREDIT_CARD);
-            transaction.setAmount(4640);
-            transaction.setCardHolderName("Phineas Flynn");
-            transaction.setCardNumber("4242424242424242");
-            transaction.setCardCvv("909");
-            transaction.setCardExpirationDate("1119");
+            transaction.setAmount(Integer.parseInt(String.valueOf(pagamento.getPedido().getPrecoTotal()).replace(".", "")));            
+            transaction.setCardId(pagamento.getTokenCartao());
             transaction.setCustomer(customer);
             transaction.save();
         } catch (PagarMeException e) {
